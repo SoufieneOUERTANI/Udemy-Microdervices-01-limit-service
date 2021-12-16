ea24ccd61163173fd8c6e6b71a470f44a0b01bc2
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2021-12-16 16:45:19 +0100

    Step 03 - Enhance limits service to pick up configuration from application properties

--- src/main/java/com/ouertani/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/com/ouertani/microservices/limitsservice/controller/LimitsController.java
@@ -1,5 +1,7 @@
 package com.ouertani.microservices.limitsservice.controller;
 
+import com.ouertani.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +9,16 @@ import com.ouertani.microservices.limitsservice.bean.Limits;
 
 @RestController
 public class LimitsController {
-	
+
+	private final Configuration configuration;
+
+	@Autowired
+	public LimitsController(Configuration configuration) {
+		this.configuration = configuration;
+	}
+
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1,1000);
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
 	}
 }
